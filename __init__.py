4264d066a002c2fb465111e1ed7325f6bdcd6121
passivestar <60579014+passivestar@users.noreply.github.com>
2024-04-19 10:49:06 +0400

    Fix #12: Multiple instances at the same position

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from functools import reduce
 
 bl_info = {
   'name': 'QuickMenu',
-  'version': (3, 0, 4),
+  'version': (3, 0, 5),
   'author': 'passivestar',
   'blender': (4, 1, 0),
   'location': 'Press the hotkey in 3D View',
