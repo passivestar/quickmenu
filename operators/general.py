5cfec736efa80980d051b47ad7d27e4872f66882
passivestar <60579014+passivestar@users.noreply.github.com>
2024-10-31 04:20:21 +0400

    Add simple rename operator

--- operators/general.py
+++ operators/general.py
@@ -194,22 +194,40 @@ class CorrectAttributesOperator(bpy.types.Operator):
     self.report({'INFO'}, 'Correct Face Attributes: ' + ('On' if ts.use_transform_correct_face_attributes else 'Off'))
     return {'FINISHED'}
 
+class RenameOperator(bpy.types.Operator):
+  """Rename"""
+  bl_idname = 'qm.rename'
+  bl_label = 'Rename'
+  bl_options = {'REGISTER', 'UNDO'}
+
+  name: bpy.props.StringProperty(name='Name')
+
+  def invoke(self, context, event):
+    return context.window_manager.invoke_props_dialog(self)
+
+  def execute(self, context):
+    for obj in context.selected_objects:
+      obj.name = self.name
+    return {'FINISHED'}
+
 def register():
-    bpy.utils.register_class(ViewOperator)
-    bpy.utils.register_class(JoinSeparateOperator)
-    bpy.utils.register_class(LocalViewOperator)
-    bpy.utils.register_class(SetSmoothOperator)
-    bpy.utils.register_class(SetOriginOperator)
-    bpy.utils.register_class(ProportionalEditingOperator)
-    bpy.utils.register_class(WireframeOperator)
-    bpy.utils.register_class(CorrectAttributesOperator)
+  bpy.utils.register_class(ViewOperator)
+  bpy.utils.register_class(JoinSeparateOperator)
+  bpy.utils.register_class(LocalViewOperator)
+  bpy.utils.register_class(SetSmoothOperator)
+  bpy.utils.register_class(SetOriginOperator)
+  bpy.utils.register_class(ProportionalEditingOperator)
+  bpy.utils.register_class(WireframeOperator)
+  bpy.utils.register_class(CorrectAttributesOperator)
+  bpy.utils.register_class(RenameOperator)
 
 def unregister():
-    bpy.utils.unregister_class(ViewOperator)
-    bpy.utils.unregister_class(JoinSeparateOperator)
-    bpy.utils.unregister_class(LocalViewOperator)
-    bpy.utils.unregister_class(SetSmoothOperator)
-    bpy.utils.unregister_class(SetOriginOperator)
-    bpy.utils.unregister_class(ProportionalEditingOperator)
-    bpy.utils.unregister_class(WireframeOperator)
-    bpy.utils.unregister_class(CorrectAttributesOperator)
+  bpy.utils.unregister_class(ViewOperator)
+  bpy.utils.unregister_class(JoinSeparateOperator)
+  bpy.utils.unregister_class(LocalViewOperator)
+  bpy.utils.unregister_class(SetSmoothOperator)
+  bpy.utils.unregister_class(SetOriginOperator)
+  bpy.utils.unregister_class(ProportionalEditingOperator)
+  bpy.utils.unregister_class(WireframeOperator)
+  bpy.utils.unregister_class(CorrectAttributesOperator)
+  bpy.utils.unregister_class(RenameOperator)
